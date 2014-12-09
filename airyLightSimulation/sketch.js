98ca9f33131d8705c0a07b49295a0b778e8571ff
aberner6 <annelieberner@gmail.com>
2014-12-09 16:14:20 -0500

    update

--- airyLightSimulation/sketch.js
+++ airyLightSimulation/sketch.js
@@ -16,9 +16,9 @@ function setup() {
   // hue, saturation, and brightness
   // colorMode(HSB, 255);
   // slider has a range between 0 and 255 with a starting value of 127
-  sliderPositionLens = createSlider(0, wallHeight, wallHeight);
+  sliderPositionLens = createSlider(wallHeight/1.2, wallHeight, wallHeight);
 
-  sliderPositionLight = createSlider(0, wallHeight, wallHeight);
+  // sliderPositionLight = createSlider(0, wallHeight, wallHeight);
 
 }
 
@@ -29,10 +29,14 @@ function draw() {
   // Set the hue according to the slider
   stroke(0, sliderPositionLens.value(), 255);
   fill(0, sliderPositionLens.value(), 255, 127);
-  ellipse(200, sliderPositionLens.value(), 20, 20);
+  ellipse(200, sliderPositionLens.value()-40, 20, 20);
 
-radius = map(sliderPositionLens.value(),0,wallHeight,-100,wallHeight/2.5);
+radius = map(sliderPositionLens.value(),wallHeight/1.2,wallHeight,10,wallHeight/2.5);
+
+// radius = map(sliderPositionLens.value(),0,wallHeight,-100,wallHeight/2.5);
 // radius = sliderPositionLens.value()/5;
+// console.log(radius+"radius");
+// console.log(sliderPositionLens.value()+"slider"); //300
   for(var i = 0; i < 100;){
     stroke(0);
    strokeWeight(.1);
@@ -40,8 +44,12 @@ radius = map(sliderPositionLens.value(),0,wallHeight,-100,wallHeight/2.5);
     ellipse(200-75+i,85,radius,radius);
     ellipse(200,10+i,radius,radius);
   }
+
+  strokeWeight(2);
+  stroke(170); 
+  line(0, wallHeight, 210, wallHeight); 
+  // line(0, sliderPositionLight.value(), 210, sliderPositionLight.value()); 
   fill(178);
   stroke(255);
-  line(0, sliderPositionLight.value(), 200, sliderPositionLight.value());  
-  ellipse(200, wallHeight, 20, 20);
+  rect(200-10, wallHeight-18, 20, 20);
 }
