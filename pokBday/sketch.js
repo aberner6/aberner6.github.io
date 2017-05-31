3ce7973294c7a0cd8c926ba4b981e76afc46ddda
Annelie <a.berner@ciid.dk>
2017-05-31 17:09:47 +0200

    asd

--- pokBday/sketch.js
+++ pokBday/sketch.js
@@ -1,45 +1,65 @@
 
-var table;
 var words = [];
-function preload() {
-  table = loadTable("peter.csv", "csv", "header");
-}
+var types = [];
+var data = [];
 var svgMain;
-function setup() {
-  // createCanvas(screen.width, screen.height);
-  var w = screen.width;
-  var h = screen.height;
-  svgMain = d3.select("#container").append("svg")
+
+  var w = 1200;
+  var h = 900;
+  svgMain = d3.select("body").append("svg")
     .attr("width",w).attr("height",h)
     .attr("class","mainSVG"); 
-
-    words.push(table.getColumn("Line"))
-    drawOne();
+first();
+function first() {
+  console.log("in here1")
+  load();
+}
+function load() {
+  d3.csv("peter.csv", function(d){
+    data.push(d);
+    for (i=0; i<d.length; i++){
+      words.push(data[0][i].Line)
+      types.push(data[0][i].ID);
+    }
+    onedraw();
+  })
 }
 var oneWord;
-function drawOne(){
-  var r = Math.round(random(0, words.length)*100);
-  oneWord = words[0][r];  // select random word
-  svgMain.append("text")
-    .attr("class","svgText")
-    .attr("x",10)
-    .attr("y",50)
-    .text(oneWord);
+var voice;
+function onedraw(){
+  console.log("in here2")
+  $("body").on("click", function(){
+    console.log("in here2")
+    d3.selectAll('.svgText').remove()
 
-  // text(oneWord,10,50);  // draw the word
-  console.log(words);
-  // if(responsiveVoice.voiceSupport()){
-  speakOne();
-  // }
+    var r = Math.round(Math.random(0, data.length));
+    console.log(r);
+    
+    oneWord = words[r];  // select random word
+    voice = types[r];
+    
+    svgMain.append("text")
+      .attr("class","svgText")
+      .attr("x",10)
+      .attr("y",50)
+      .text(oneWord);
+    speakOne(oneWord);
+    
+    console.log(oneWord);
+    console.log(voice);
+  })
 }
 
-function speakOne(){
-$('.svgText').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
-  responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
-  responsiveVoice.speak($(this).text()); // Speak the text contents of all nodes within the current 'a' tag
-});
-  // setTimeout(responsiveVoice.speak(oneWord)
-  // ,15000);
-  // responsiveVoice.speak(oneWord, "UK English Male", {volume: 1});
-  // responsiveVoice.speak(oneWord, "UK Male", {rate: 1.5});
+function speakOne(oneWord, voice){
+  console.log("here")
+  var voiceStyle;
+  if(voice=="Male"){
+    voiceStyle = "UK English Male";
+  }else{
+    voiceStyle = "US English Female";
+  }
+  $('.svgText').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
+    responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
+    responsiveVoice.speak(oneWord, voiceStyle); // Speak the text contents of all nodes within the current 'a' tag
+  });
 }
